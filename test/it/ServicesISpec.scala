f36723cc204d98d013d80490cf79d32602e43fb1
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2020-09-23 16:41:11 +0100

    PLATUI-743: Added additional integration tests to check parsing and validation of YAML files.

--- test/it/ServicesISpec.scala
+++ test/it/ServicesISpec.scala
@@ -16,11 +16,13 @@
 
 package it
 
+import java.io.File
+
 import cats.syntax.either._
 import org.scalatest.TryValues
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import uk.gov.hmrc.accessibilitystatementfrontend.config.{ServicesFinder, SourceConfig}
+import uk.gov.hmrc.accessibilitystatementfrontend.config.{AppConfig, ServicesFinder, SourceConfig}
 import uk.gov.hmrc.accessibilitystatementfrontend.models.AccessibilityStatement
 import uk.gov.hmrc.accessibilitystatementfrontend.parsers.AccessibilityStatementParser
 
@@ -29,25 +31,55 @@ import scala.util.Try
 class ServicesISpec extends PlaySpec with GuiceOneAppPerSuite with TryValues {
   private val statementParser = new AccessibilityStatementParser
 
-  "the configuration files" should {
-    val sourceConfig   = app.injector.instanceOf[SourceConfig]
+  "parsing the configuration files" should {
+    val sourceConfig = app.injector.instanceOf[SourceConfig]
     val servicesFinder = app.injector.instanceOf[ServicesFinder]
 
     servicesFinder.findAll().foreach { (service: String) =>
       val source = sourceConfig.statementSource(service)
-
       val statementTry = Try(statementParser.parseFromSource(source).valueOr(throw _))
 
-      s"include a correctly formatted accessibility statement yaml file for $service" in {
+      s"enforce a correctly formatted accessibility statement yaml file for $service" in {
         statementTry must be a 'success
       }
 
-      s"not contain missing milestones for $service" in {
+      s"enforce statement not contain missing milestones for $service" in {
         val statement: AccessibilityStatement = statementTry.get
-
         val hasMilestones = statement.milestones.getOrElse(Seq.empty).nonEmpty
-
         hasMilestones || statement.isNonCompliant || statement.isFullyCompliant must be(true)
+      }
+
+      s"enforce serviceDomain in the format of aaaa.bbbb.cccc for $service" in {
+        val domainRegex = "([a-z0-9-]*[\\.]*)*[a-z0-9]*"
+        val statement: AccessibilityStatement = statementTry.get
+        statement.serviceDomain.matches(domainRegex) must be(true)
+      }
+
+      s"enforce serviceUrl starting with / for $service" in {
+        val statement: AccessibilityStatement = statementTry.get
+        statement.serviceUrl.startsWith("/") must be(true)
+      }
+    }
+  }
+
+  "the file names in the directory" should {
+    val servicesFinder = app.injector.instanceOf[ServicesFinder]
+    val appConfig = app.injector.instanceOf[AppConfig]
+
+    val servicesDirectoryPath =
+      new File(getClass.getClassLoader.getResource(appConfig.servicesDirectory).getPath)
+    val fileNames =
+      servicesDirectoryPath.listFiles().toSeq.filter(_.isFile).map(_.getName).sorted
+
+    fileNames.foreach { fileName =>
+      s"end in extension .yml for $fileName" in {
+        fileName.endsWith(".yml") must be(true)
+      }
+
+      s"should match to a service returned by the service finder for $fileName" in {
+        val serviceName = fileName.split("\\.").head
+        val services = servicesFinder.findAll()
+        services.contains(serviceName) must be(true)
       }
     }
   }
