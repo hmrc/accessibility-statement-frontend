818963875fef08cba25e5cc01deb2ab5359c0686
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2021-05-14 15:10:15 +0100

    PLATUI-998: Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,8 +7,8 @@ object AppDependencies {
   private val circeVersion = "0.12.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "4.1.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "0.56.0-play-27",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "5.2.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "0.63.0-play-27",
     "io.circe"    %% "circe-core"                 % circeVersion,
     "io.circe"    %% "circe-generic"              % circeVersion,
     "io.circe"    %% "circe-parser"               % circeVersion,
@@ -16,7 +16,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-27"  % "4.1.0"  % "test",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-27"  % "5.2.0"  % "test",
     "org.scalatest"          %% "scalatest"               % "3.0.8"  % "test",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "4.0.3"  % "test",
     "org.mockito"            %% "mockito-scala-scalatest" % "1.14.8" % "test",
