603210bccdedc90b03fe0c123654afefa78cb0e9
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2022-07-18 11:57:51 +0100

    PLATUI-1853: Spike adding native app install prompy

---
+++ public/app-install-prompt.js
@@ -0,0 +1,10 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can add to home screen
+    console.log("beforeinstallprompt fired!")
+});
