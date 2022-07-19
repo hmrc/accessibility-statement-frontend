3bfda7e44d5bb409f2b77c158d6d3338dc8f4045
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2022-07-19 11:31:15 +0100

    PLATUI-1853: Updated Javascript

--- public/app-install-prompt.js
+++ public/app-install-prompt.js
@@ -8,3 +8,16 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Update UI notify the user they can add to home screen
     console.log("beforeinstallprompt fired!")
 });
+
+window.addEventListener('load', (e) => {
+    console.log('page is fully loaded');
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        // Update UI notify the user they can add to home screen
+        console.log("beforeinstallprompt fired!")
+    });
+});
