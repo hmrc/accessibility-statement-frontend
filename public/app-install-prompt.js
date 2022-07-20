93348ee3cb55ca818703a4f7f985c7b8d5c45431
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2022-07-20 14:33:21 +0100

    PLATUI-1853: Updated event log to be alerts

--- public/app-install-prompt.js
+++ public/app-install-prompt.js
@@ -1,16 +1,7 @@
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can add to home screen
-    console.log("beforeinstallprompt fired!")
-});
-
 window.addEventListener('load', (e) => {
-    console.log('page is fully loaded');
+    window.alert('page load called')
 
     window.addEventListener('beforeinstallprompt', (e) => {
         // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -18,6 +9,6 @@ window.addEventListener('load', (e) => {
         // Stash the event so it can be triggered later.
         deferredPrompt = e;
         // Update UI notify the user they can add to home screen
-        console.log("beforeinstallprompt fired!")
+        window.alert("beforeinstallprompt fired!")
     });
 });
