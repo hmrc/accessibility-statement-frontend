8300a3a2adf8518ef2f50ef50dca547e07ea5309
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2022-11-14 12:36:43 +0000

    NOJIRA: Updated various dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.11.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.24.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.33.0-play-28",
     "io.circe"    %% "circe-core"                 % circeVersion,
     "io.circe"    %% "circe-generic"              % circeVersion,
     "io.circe"    %% "circe-parser"               % circeVersion,
@@ -13,7 +13,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "6.2.0"    % "test",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "7.11.0"   % "test",
     "org.scalatest"          %% "scalatest"               % "3.2.13"   % "test",
     "org.scalatestplus"      %% "selenium-4-2"            % "3.2.13.0" % "test",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"    % "test",
