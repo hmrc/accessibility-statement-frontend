447aae9d844eb87294be032bfa09fc32ecb94fe5
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-01-04 15:25:40 +0000

    NOJIRA: Bumping app dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,11 +1,12 @@
 import sbt._
 
 object AppDependencies {
-  private val circeVersion = "0.14.1"
+  private val circeVersion     = "0.14.1"
+  private val bootstrapVersion = "7.12.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.11.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.33.0-play-28",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "5.5.0-play-28",
     "io.circe"    %% "circe-core"                 % circeVersion,
     "io.circe"    %% "circe-generic"              % circeVersion,
     "io.circe"    %% "circe-parser"               % circeVersion,
@@ -13,13 +14,13 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "7.11.0"   % "test",
-    "org.scalatest"          %% "scalatest"               % "3.2.13"   % "test",
-    "org.scalatestplus"      %% "selenium-4-2"            % "3.2.13.0" % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"    % "test",
-    "org.mockito"            %% "mockito-scala-scalatest" % "1.14.8"   % "test",
-    "org.jsoup"               % "jsoup"                   % "1.10.2"   % "test",
-    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.38.0"   % "test",
-    "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"   % "test"
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVersion % "test",
+    "org.scalatest"          %% "scalatest"               % "3.2.13"         % "test",
+    "org.scalatestplus"      %% "selenium-4-2"            % "3.2.13.0"       % "test",
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"          % "test",
+    "org.mockito"            %% "mockito-scala-scalatest" % "1.14.8"         % "test",
+    "org.jsoup"               % "jsoup"                   % "1.10.2"         % "test",
+    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.41.0"         % "test",
+    "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"         % "test"
   )
 }
