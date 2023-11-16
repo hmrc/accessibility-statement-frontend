23736790b52ba427af8d7d9642059296487cfed5
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-11-16 11:16:34 +0000

    NOJIRA: Update acceptance tests script to run via Docker

--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -1,2 +1,25 @@
-#!/usr/bin/env bash
-sbt -Dbrowser=chrome acceptance:test
+#!/bin/bash -e
+BROWSER="remote-chrome"
+ENV="local"
+
+port_mappings="6001->6001,12346->12346"
+
+IMAGE=artefacts.tax.service.gov.uk/chrome-with-rinetd:latest
+
+# When using on a Linux OS, add "--net=host" to the docker run command.
+docker pull ${IMAGE} \
+  && docker run \
+  -d \
+  --rm \
+  --name "remote-chrome" \
+  --shm-size=2g \
+  -p 4444:4444 \
+  -p 5900:5900 \
+  -e PORT_MAPPINGS="$port_mappings" \
+  -e TARGET_IP='host.docker.internal' \
+  ${IMAGE}
+
+sbt \
+  -Dbrowser=$BROWSER \
+  -Denvironment=$ENV \
+  acceptance:test
