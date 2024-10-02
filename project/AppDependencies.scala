ec93aedf9d9b752db2d1ab205ec9e33aac9510fc
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2024-10-02 11:55:23 +0100

    PLATUI-3265 bump bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVersion = "9.4.0"
+  private val bootstrapVersion = "9.5.0"
   private val frontendVersion  = "10.12.0"
   private val playVersion      = "play-30"
 
