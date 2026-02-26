0e56f28de0b019b66b7be73c49f8450a6b385926
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2026-02-26 08:01:16 +0000

    PLATUI-4178 bump sbt-plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.6.0"
   private val frontendVersion  = "12.31.0"
   private val playVersion      = "play-30"
 
